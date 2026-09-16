f37c4adbe203601ee16e061a2acc73ae69f21f10
Guanlanzhe <25801980033@m.fudan.edu.cn>
2026-09-16 17:46:22 +0800

    Remove unused PORT constant from server.js

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
